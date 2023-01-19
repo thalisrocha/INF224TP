6bf9d89819945dd45ea1b7e113ead4153e91cf0b
Guilhem Jazeron <gjazeron-21@crdn-10.enst.fr>
2023-01-19 15:24:01 +0100

    début étape 10

---
+++ DataBase.h
@@ -0,0 +1,22 @@
+#ifndef DATABASE
+#define DATABASE
+
+#include "MultimediaObject.h"
+#include "Picture.h"
+#include "Video.h"
+#include "Film.h"
+#include "Group.h"
+#include <memory>
+#include <string>
+#include <map>
+
+using GroupPtr = std::shared_ptr<Group>;
+
+
+class DataBase{
+    
+    private :
+
+};
+
+#endif
